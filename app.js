ce8d95298add202e71ce6a9ffff17938ea0040cd
Uriel Zacarias <urielzacarias@gmail.com>
2022-12-15 20:56:39 -0800

    updated deploy

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 //setup 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 //dev packages
 if (process.env.NODE_ENV !== 'production') {
-    //development logging
-    const morgan = require('morgan');
-    app.use(morgan('dev'));
+	//development logging
+	const morgan = require('morgan');
+	app.use(morgan('dev'));
 }
 
 // config
--- server.js
+++ server.js
@@ -7,48 +7,48 @@ const express = require('express');
 mongoose.Promise = global.Promise;
 mongoose.set('strictQuery', false);
 mongoose.connect(keys.mongoURI, { 
-    autoIndex: process.env.NODE_ENV === 'production' ? false : true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+	autoIndex: process.env.NODE_ENV === 'production' ? false : true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true
 })
-  .then(res => console.log('Succesfully connected to MongoDb'))
-  .catch(err => console.log('💥Failed to connect to MongoDb', err))
-module.exports = {mongoose}
+	.then(() => console.log('Succesfully connected to MongoDb'))
+	.catch(err => console.log('💥Failed to connect to MongoDb', err));
+module.exports = {mongoose};
 
 //serve production files
 if (process.env.NODE_ENV === 'production') {
-    //Express will server up production asses
-    app.use(express.static('./frontend/build'));
+	//Express will server up production asses
+	app.use(express.static('./frontend/build'));
 
-    //Express will serve the index.html file
-    //if it doesn't recogize the route
-    const path = require('path');
-    const filepath = path.join(__dirname, './frontend/build/index.html');
+	//Express will serve the index.html file
+	//if it doesn't recogize the route
+	const path = require('path');
+	const filepath = path.join(__dirname, './frontend/build/index.html');
 
-    app.get('*', (req,res) => {
-        res.sendFile(filepath, function(err){
-            if (err) return res.status(err.status).end();
-            else return res.status(200).end();
-        });
-    });
-};
+	app.get('*', (req,res) => {
+		res.sendFile(filepath, function(err){
+			if (err) return res.status(err.status).end();
+			else return res.status(200).end();
+		});
+	});
+}
 
 //start server
-const server = app.listen(keys.port, keys.ipAdress, (err) =>{
-    console.log('App running on port: ', keys.port);
+const server = app.listen(keys.port, keys.ipAdress, () =>{
+	console.log('App running on port: ', keys.port);
 });
 //handle errors
 process.on('unhandledRejection', err => {
-    console.log('UNHANDLED REJECTION! 💥 Shutting down...');
-    console.log(err.name, err.message);
-    server.close(() => {
-        process.exit(1);
-    });
+	console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+	console.log(err.name, err.message);
+	server.close(() => {
+		process.exit(1);
+	});
 });
   
 process.on('SIGTERM', () => {
-    console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
-    server.close(() => {
-        console.log('💥 Process terminated!');
-    });
+	console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
+	server.close(() => {
+		console.log('💥 Process terminated!');
+	});
 });

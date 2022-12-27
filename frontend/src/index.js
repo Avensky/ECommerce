43aa5ce28102769d2746ddfe6c5cd0198a020a2c
Uriel Zacarias <urielzacarias@gmail.com>
2022-12-26 21:10:23 -0800

    updated index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
@@ -36,11 +36,13 @@ faTrashCan,
 faPlus,
 faMinus
 );
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+const app = (
 //  <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
 //  </React.StrictMode>
 );
+
+ReactDOM.render(app,document.getElementById('root'));

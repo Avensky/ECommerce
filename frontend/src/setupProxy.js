b54e474a750e4dcebd3ca27ff49a1856cfd7114a
Uriel Zacarias <urielzacarias@gmail.com>
2023-01-08 10:59:39 -0800

    updated app

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,6 +1,7 @@
 const keys = require('./config/keys');
 const {createProxyMiddleware} = require('http-proxy-middleware');
 
+console.log('proxy',keys.proxyTarget)
 module.exports = function (app) {
   app.use(
     ["/api"],

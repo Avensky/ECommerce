3883dbda3c50327458d5055dc97b2fe86420bd29
Uriel Zacarias <urielzacarias@gmail.com>
2023-01-09 01:01:52 -0800

    updated server

--- controllers/passport.ts
+++ controllers/passport.ts
@@ -2,7 +2,7 @@
 // load all the things we need =================================================
 // =============================================================================
 const LocalStrategy    = require('passport-local').Strategy;
-//const FacebookStrategy = require('passport-facebook').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 //const TwitterStrategy  = require('passport-twitter').Strategy;
 const GoogleStrategy   = require('passport-google-oauth20').Strategy;
 const mongoose         = require('mongoose')
@@ -228,88 +228,87 @@ module.exports         = function(passport:any) {
 //
 //    }));
 //
-//
-    // =========================================================================
-    // FACEBOOK ================================================================
-    // =========================================================================
-//    passport.use(new FacebookStrategy({
-//        clientID            : keys.facebookClientID,
-//        clientSecret        : keys.facebookClientSecret,
-//        callbackURL         : keys.facebookCallbackURL,
-//        passReqToCallback   : true, // allows us to pass in the req from our route (lets us check if a user is logged in or not)
-//        profileFields       : ['id', 'displayName', 'photos', 'email','first_name', 'last_name'],
-////        enableProof     : true,
-//        proxy           : true
-//    },
-//    function (req, token, refreshToken, profile, done) {
-//
-//        // asynchronous
-//        process.nextTick(function() {
-//
-//            // check if the user is already logged in
-//            if (!req.user) {
-//
-//                User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
-//
-//                    // if there is an error, stop everything and return that
-//                    // ie an error connecting to the database
-//                    if (err)
-//                        return done(err);
-//
-//                    // if the user is found, then log them in
-//                    if (user) {
-//
-//                        // if there is a user id already but no token (user was linked at one point and then removed)
-//                        // just add our token and profile information
-//                        if (!user.facebook.token) {
-//                            user.facebook.token = token;
-//                            user.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
-//                            user.facebook.email = profile.emails[0].value;
-//
-//                            user.save(function(err) {
-//                                if (err)
-//                                    throw err;
-//                                return done(null, user);
-//                            });
-//                        }
-//
-//                        return done(null, user); // user found, return that user
-//                    } else {
-//                        // if there is no user, create them
-//                        var newUser            = new User();
-//
-//                        newUser.facebook.id    = profile.id;
-//                        newUser.facebook.token = token;
-//                        newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
-//                        newUser.facebook.email = profile.emails[0].value;
-//
-//                        newUser.save(function(err) {
-//                            if (err)
-//                                throw err;
-//                            return done(null, newUser);
-//                        });
-//                    }
-//                });
-//
-//            } else {
-//                // user already exists and is logged in, we have to link accounts
-//                var user            = req.user; // pull the user out of the session
-//
-//                user.facebook.id    = profile.id;
-//                user.facebook.token = token;
-//                user.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
-//                user.facebook.email = profile.emails[0].value;
-//
-//                user.save(function(err) {
-//                    if (err)
-//                        throw err;
-//                    return done(null, user);
-//                });
-//
-//            }
-//        });
-//    }));
-//
+// =========================================================================  
+// FACEBOOK ================================================================  
+// =========================================================================
+    passport.use(new FacebookStrategy({
+        clientID            : keys.facebookClientID,
+        clientSecret        : keys.facebookClientSecret,
+        callbackURL         : keys.facebookCallbackURL,
+        passReqToCallback   : true, // allows us to pass in the req from our route (lets us check if a user is logged in or not)
+        profileFields       : ['id', 'displayName', 'photos', 'email','first_name', 'last_name'],
+//        enableProof     : true,
+        proxy           : true
+    },
+    function (req:any, token:any, refreshToken:any, profile:any, done:any) {
+
+        // asynchronous
+        process.nextTick(function() {
+
+            // check if the user is already logged in
+            if (!req.user) {
+
+                Users.findOne({ 'facebook.id' : profile.id }, function(err:any, user:any) {
+
+                    // if there is an error, stop everything and return that
+                    // ie an error connecting to the database
+                    if (err)
+                        return done(err);
+
+                    // if the user is found, then log them in
+                    if (user) {
+
+                        // if there is a user id already but no token (user was linked at one point and then removed)
+                        // just add our token and profile information
+                        if (!user.facebook.token) {
+                            user.facebook.token = token;
+                            user.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
+                            user.facebook.email = profile.emails[0].value;
+
+                            user.save(function(err:any) {
+                                if (err)
+                                    throw err;
+                                return done(null, user);
+                            });
+                        }
+
+                        return done(null, user); // user found, return that user
+                    } else {
+                        // if there is no user, create them
+                        var newUser            = new Users();
+
+                        newUser.facebook.id    = profile.id;
+                        newUser.facebook.token = token;
+                        newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
+                        newUser.facebook.email = profile.emails[0].value;
+
+                        newUser.save(function(err:any) {
+                            if (err)
+                                throw err;
+                            return done(null, newUser);
+                        });
+                    }
+                });
+
+            } else {
+                // user already exists and is logged in, we have to link accounts
+                var user            = req.user; // pull the user out of the session
+
+                user.facebook.id    = profile.id;
+                user.facebook.token = token;
+                user.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
+                user.facebook.email = profile.emails[0].value;
+
+                user.save(function(err:any) {
+                    if (err)
+                        throw err;
+                    return done(null, user);
+                });
+
+            }
+        });
+    }));
+
 //    // =========================================================================
 //    // TWITTER =================================================================
 //    // =========================================================================

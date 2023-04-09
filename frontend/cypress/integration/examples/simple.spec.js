7dd20fa2ccfbb151aef4830c90f494c280216f9b
Uriel Zacarias <urielzacarias@gmail.com>
2023-04-08 20:41:04 -0700

    delete sample test

--- frontend/cypress/integration/examples/simple.spec.js
+++
@@ -1,8 +0,0 @@
-describe('My first Cypress Test', ()=>{
-    it('Load correct url', ()=>{
-        cy.visit('https://urielzacarias.com/', {timeout:10000});
-    });
-//    it('should check correct url', ()=>{
-//        cy.url().should('include', 'avensky');
-//    });
-});
